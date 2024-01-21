0795f6679f3236c2fe8a29eef34af0108d06184e
ziaasifmuhammad <155935485+ziaasifmuhammad@users.noreply.github.com>
2024-01-21 19:28:11 +0500

    Add files via upload

---
+++ indus.js
@@ -0,0 +1,23 @@
+/*-----------
+if age is grater then 22 :mystudent*/
+
+alert('wellcome to zia class for upcoming lecture about javascript');
+
+var Age = prompt('Please enter age');
+var mystudent = prompt('name of student');
+
+
+
+
+console.log("Age");
+console.log("mystudent");
+
+if(age<25){
+    console.log('you are a student');
+} else {
+    console.log('not eligable');
+}
+ 
+    
+
+
---
+++ zia.js
@@ -0,0 +1,7 @@
+/*--Area = width * length)*2---*/
+alert('wellcome to zia class, what you want to learn');
+var  width = prompt ('Please enter the width value');
+var length = prompt ('Please enter the length value');
+var area = (width * length) * 2;
+console.log(area);
+
